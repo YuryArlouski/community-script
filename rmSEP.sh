df452b840e4150201fb996476e1c505f61cb686d
Yury Arlouski <50956064+YuryArlouski@users.noreply.github.com>
2025-03-13 14:50:11 +0300

    Create rmSEP.sh

---
+++ rmSEP.sh
@@ -0,0 +1,3 @@
+curl https://github.com/YuryArlouski/community-script/blob/main/RemoveSymantecMacFiles.command -o /tmp/RemoveSymantecMacFiles.sh 
+sudo chmod 700 /tmp/RemoveSymantecMac.sh
+/tmp/RemoveSymantecMac.sh
